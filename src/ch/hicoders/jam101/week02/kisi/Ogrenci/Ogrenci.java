863315fb2d74105a7f3a00d0e345bff1aae51eee
Mehmet Sökmen <sokmen4083@gmail.com>
2020-12-12 14:48:54 +0100

    week 3-School has done

---
+++ src/ch/hicoders/jam101/week02/kisi/Ogrenci/Ogrenci.java
@@ -0,0 +1,15 @@
+package ch.hicoders.jam101.week02.kisi.Ogrenci;
+
+
+public class Ogrenci {
+public int ogrenciIsmi;
+public int almancaNotu;
+public int matematikNotu;
+
+
+    public Ogrenci(int pOgrenciIsmi, int pAlmancaNotu,int pMatematikNotu ){
+        this.ogrenciIsmi = pOgrenciIsmi;
+        this.almancaNotu = pAlmancaNotu;
+        this.matematikNotu = pMatematikNotu;
+    }
+}
